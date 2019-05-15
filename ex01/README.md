89ba13b230f385da94bbf857c8ab6ea989e74fcc
alinetskyi <artem.linetskiy@gmail.com>
2019-05-15 22:24:13 +0300

    Initialize repository structure

---
+++ ex01/README.md
@@ -0,0 +1,7 @@
+#Implement Caesar cipher in any given language
+The structure of the cli program should be as follows
+
+caesar -k (key) (input)
+with the following options:
+-d for decrypt 
+-e for encrypt
---
+++ ex01/caesar_test.go
@@ -0,0 +1,56 @@
+package main
+
+import ( 
+	"testing"
+	"os/exec"
+)
+
+var testCases = []struct {
+	input    string
+	key		 int
+	expected string
+}{
+	{
+		input:    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		key:	  23
+		expected: "XYZABCDEFGHIJKLMNOPQRSTUVW",
+	},
+	{
+		input:    "ATTACKATONCE",
+		key:	  4 
+		expected: "EXXEGOEXSRGI",
+	},
+	{
+		input:    "cipher",
+		key:	  6 
+		expected: "iovnkx",
+	},
+	{
+		input:    "caesar",
+		key:	  15 
+		expected: "rpthpg",
+	},
+	{
+		input:    "TESTlolKek",
+		key:	  19 
+		expected: "MXLMeheDxd",
+	},
+}
+
+func TestCaesar(t *testing.T) {
+	var actual bytes.Buffer
+	for _, tt := range testCases {
+		command := fmt.Sprintf("caesar -e --key %d %s", tt.key, tt.input)	
+		caesar := exec.Command("caesar lol 3")
+		caesar.Stdout = &actual
+		err := caesar.Run()
+
+		if err != nil {
+			t.Fatalf("Caesar(%q) returned error %q.  Error not expected.", tt.input, err)
+		}
+		if actual != tt.expected {
+			t.Fatalf("Caesar(%q) was expected to return %v but returned %v.",
+				tt.input, tt.expected, actual)
+		}
+	}
+}
